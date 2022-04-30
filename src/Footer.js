168fa5a83ef6f66fac6e2fee999306d5108e326b
Anael961 <98133723+Anael961@users.noreply.github.com>
2022-04-30 19:35:13 -0400

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,11 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <small className="Footer">
-      <a href="github" target="_blank">
+      <a
+        href="https://github.com/Anael961/weather-react"
+        rel="noreferrer"
+        target="_blank"
+      >
         Open-source code
       </a>{" "}
       <span className="author"> by Elodie M </span>
